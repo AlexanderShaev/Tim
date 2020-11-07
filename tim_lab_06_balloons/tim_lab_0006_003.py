ce062d68a54316b0a0fb3699095a38a61d44ee78
AlexsanderShaev <arhd@mail.ru>
2020-11-07 15:09:19 +0300

    made circles

--- tim_lab_06_balloons/tim_lab_0006_003.py
+++ tim_lab_06_balloons/tim_lab_0006_003.py
@@ -10,7 +10,7 @@ from random import randint # импорт функции randint, randint(start,
 from pygame.draw import * # чтобы не писать pygame.draw.circle а только circle
 pygame.init() # инициализация библиотеки
 
-fps = 30 # частота обновления экрана
+fps = 1 # частота обновления экрана
 screen = pygame.display.set_mode((800, 800)) # создать окно
 
 
@@ -27,19 +27,43 @@ colors = [red, blue, yellow, green, magenta, cyan]
 
 def new_ball():
     '''
-    функция рисует новый шарик
+    функция рисует новый круг
 
     Returns 
     -------
     None.
 
     '''
+    global x, y, r # глобальные переменные
     x = randint(100, 1100)
     y = randint(100, 900)
     r = randint(10, 100)
     color = colors[randint(0, 5)]
     circle(screen, color, (x, y), r)
 
+
+def click(event):
+    print(x, y, r)
+    
+
+def compare():
+    '''
+    функция сравнивает место клика мышью и радиус круга
+    и определеет "Попал!!!" или "Промазал"
+
+    Returns
+    -------
+    None.
+
+    '''
+    shot = event.pos
+    print('Выстрел --------------------', shot)
+    a = ((x-shot[0])**2+(y-shot[1])**2)**0.5
+    print('Расстояние до центра круга -', a)
+    if a <= r:
+        print('Попал!!!')
+    else:
+        print('Промазал!!!')
 
 pygame.display.update() # обновление экрана
 clock = pygame.time.Clock() # создание счётчика времени
@@ -52,11 +76,21 @@ while not finished: # пока не завершилось делать:
     for event in pygame.event.get(): # забирает список событий из очереди
         if event.type == pygame.QUIT: # сравнение с константой "Закрытие окна"
             finished = True # завершение программы
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            click(event)
+            print('Click -----------------', x, y, r)
+            compare()
+#            if event.x == 0:
+#                print('Попал')
+#            else:
+#                print('Промазал')
+                
 
 
     new_ball() # новый круг
     pygame.display.update() # обновить окно
 #    screen.fill(black) # залить всё окно одним цветом
+    click(event)
     
 
 pygame.quit()
