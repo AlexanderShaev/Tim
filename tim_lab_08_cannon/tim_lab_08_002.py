8627beffc933005b6b1ae1b1717014e15f3cfae6
AlexsanderShaev <arhd@mail.ru>
2020-11-08 19:09:28 +0300

    cannon start

---
+++ tim_lab_08_cannon/tim_lab_08_002.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  8 16:41:44 2020
+
+@author: ccc
+"""
+
+
+
+class GameRound: # игровой раунд
+   pass
+    
+class Cannon: # пушка
+    pass
+
+class Shell: # снаряд
+    pass
+
+class Target: # цель
+    pass
+
+class Bomb: # бомба
+    pass
+
+dora = Cannon()
